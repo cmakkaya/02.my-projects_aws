f97ae31afc4e535f41dcc0ca8bbd6785385d4974
cetin <cm.akkaya.aws@gmail.com>
2022-11-23 15:11:36 +0300

    Using my-aws-capstone-project-main--ALB_with_Auto_Scaling_S3_RDS_VPC_s_Components-Lambda_DynamoDB_and_Cloudfront_with_Route_53, deployed Blog Page Application (Django)

--- my-aws-capstone-project-main--ALB_with_Auto_Scaling_S3_RDS_VPC_s_Components-Lambda_DynamoDB_and_Cloudfront_with_Route_53/userdata.sh
+++ my-aws-capstone-project-main--ALB_with_Auto_Scaling_S3_RDS_VPC_s_Components-Lambda_DynamoDB_and_Cloudfront_with_Route_53/userdata.sh
@@ -3,7 +3,7 @@ apt-get update -y
 apt-get install git -y
 apt-get install python3 -y
 cd /home/ubuntu/
-TOKEN="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+TOKEN="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 git clone https://$TOKEN@github.com/cmakkaya/my-aws-capstone-project.git
 cd /home/ubuntu/my-aws-capstone-project
 apt install python3-pip -y
